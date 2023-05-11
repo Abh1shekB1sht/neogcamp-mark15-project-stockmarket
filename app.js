09bd71db885e462c6a1eeb085d8af9486122c9e9
Abhishek Bisht <singhbisht84484@gmail.com>
2023-05-11 14:06:29 +0530

    feat: added function calcuateprofit&loss
    This functions calculate profit or loss and  on the basis tells the user about that.

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ var currentPrice = document.querySelector('#current-price');
 var clickButton = document.querySelector('#click-button');
 var printMessage = document.querySelector('#print-message');
 
+function calculateProfitAndLoss() {
+    var profitOrLoss = currentPrice.value - initialPrice.value;
+    if(profitOrLoss > initialPrice.value) {
+        var percentageProfit = Math.trunc(((profitOrLoss*quantityOfStock.value)/(initialPrice.value*quantityOfStock.value))*100);
+        printMessage.innerText = "Hey, the profit is " + profitOrLoss + " and the percentage is " + percentageProfit + "%";
+    }else {
+        var percentageLoss = Math.trunc(((profitOrLoss*quantityOfStock.value)/(initialPrice.value*quantityOfStock.value))/100);
+        printMessage.innerText = "Hey, the loss is " + profitOrLoss + " and the percentage is " + percentageLoss + "%";
+    }
+}
+
+
+
 clickButton.addEventListener('click',calculateProfitAndLoss);
