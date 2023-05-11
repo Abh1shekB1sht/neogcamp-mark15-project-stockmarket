818219a4220a7aa120e8a7efcc05fbc7ab325418
Abhishek Bisht <singhbisht84484@gmail.com>
2023-05-11 20:15:44 +0530

    refactor: code update

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ function calculateProfitAndLoss() {
     var profitOrLoss = currentPrice.value - initialPrice.value;
     if(profitOrLoss > initialPrice.value) {
         var percentageProfit = Math.trunc(((profitOrLoss*quantityOfStock.value)/(initialPrice.value*quantityOfStock.value))*100);
-        printMessage.innerText = "Hey, the profit is " + profitOrLoss + " and the percentage is " + percentageProfit + "%";
+        printMessage.innerText = "Hey, the profit is " + profitOrLoss + " and the profit-percentage is " + percentageProfit + "%";
     }else {
-        var percentageLoss = Math.trunc(((profitOrLoss*quantityOfStock.value)/(initialPrice.value*quantityOfStock.value))/100);
-        printMessage.innerText = "Hey, the loss is " + profitOrLoss + " and the percentage is " + percentageLoss + "%";
+        const lossAmount = Math.abs(profitOrLoss)
+        var percentageLoss = Math.trunc(((lossAmount*quantityOfStock.value)*100)/initialPrice.value);
+        printMessage.innerText = "Hey, the loss is " + lossAmount + " and the loss-percentage is " + percentageLoss + "%";
     }
 }
 
